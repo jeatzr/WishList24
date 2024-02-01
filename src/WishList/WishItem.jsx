b8510d63c40db702a35d294a475519660938c72d
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2024-02-01 10:46:28 +0100

    Wish Item - Age state colors

--- src/WishList/WishItem.jsx
+++ src/WishList/WishItem.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function WishItem({ wish, onCompletedChange }) {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    let ageInterval;
+    if (wish.completed) {
+      // si el deseo es completado dejo de contar
+      // y limpio el intervalo
+      setAge(0);
+      clearInterval(ageInterval);
+    } else {
+      // si el deseo no está completado
+      // creo un intervalo que incrementa en 1
+      // la edad del deseo cada 1000 milisegundos
+      ageInterval = setInterval(() => {
+        setAge((currentAge) => currentAge + 1);
+      }, 1000);
+    }
+    // la función que devuelve return se ejecuta
+    //  solo cuando se desmonta el componente
+    return () => clearInterval(ageInterval);
+  }, [wish.completed]);
+
   return (
     <li
       className={`
             wish-list__item
             ${wish.completed ? 'wish-list__item--done' : ''}
+            ${age >= 5 && age < 10 ? 'wish-list__item--warn' : ''}
+            ${age >= 10 ? 'wish-list__item--danger' : ''}
             `}
     >
       <input
